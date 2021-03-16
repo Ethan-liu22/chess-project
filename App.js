e452b516db8135227fe0a84dc38894d46b742b3f
Ethan-liu22 <80666291+Ethan-liu22@users.noreply.github.com>
2021-03-16 12:52:24 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+//import './App.css';
+//import Board from './Board'
+//import ReactDOM from 'react-dom'
+//import Knight from './Knight'
+//import Square from './Square'
+import Board from './Board'
+
+
+function App() {
+  return (
+    <Board knightPosition={[7, 4]} />
+  );
+}
+export default App;
+
+
+
